8166909c9d3f15e5f6578ee301dd62d77d1bcfee
wlft <100052562+wlft@users.noreply.github.com>
2026-01-13 10:42:13 +1000

    fix(cover.ts): outdated return type

--- src/utils/cover.ts
+++ src/utils/cover.ts
@@ -1,5 +1,6 @@
 import { GetIndexFromSlugID } from "./hash";
 import aoyukiConfig from "../../aoyuki.config";
+import type { Configuration } from "../types/config";
 
 /**
  * Retrieves the cover URL for an unspecified entry based on the provided ID.
@@ -7,7 +8,7 @@ import aoyukiConfig from "../../aoyuki.config";
  * @param id - The unique identifier for the entry.
  * @returns The URL of the corresponding cover image.
  */
-export function GetCoverURLForUnspecifiedEntry(id: string): string {
+export function GetCoverURLForUnspecifiedEntry(id: string): Configuration['banners'][number] {
   const index = GetIndexFromSlugID(id, aoyukiConfig.banners.length);
   return aoyukiConfig.banners[index];
 }
