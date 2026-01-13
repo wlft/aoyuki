add78a23d4618c8ba640c4e9269a054212a84113
wlft <100052562+wlft@users.noreply.github.com>
2026-01-14 02:24:20 +1000

    import github card component from fuwari

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,13 @@ import rehypeSlug from "rehype-slug";
 import rehypeKatex from "rehype-katex";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkMath from "remark-math";
+import remarkSectionize from "remark-sectionize";
+import rehypeComponents from "rehype-components";
+import remarkDirective from "remark-directive";
+import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
+import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import { GithubCardComponent } from "./src/plugins/rehype-component-gtihub-card.mjs";
 
 import aoyukiConfig from "./aoyuki.config";
 
@@ -42,14 +48,27 @@ export default defineConfig({
     shikiConfig: {
       theme: "github-dark-default",
     },
-    remarkPlugins: [remarkReadingTime, remarkMath],
+    remarkPlugins: [remarkReadingTime, remarkMath, remarkExcerpt, remarkSectionize, remarkDirective, parseDirectiveNode],
     rehypePlugins: [
-      rehypeSlug,
       rehypeKatex,
+      rehypeSlug,
       [
         rehypeAutolinkHeadings,
         {
           behavior: "prepend",
+        },
+      ],
+      [
+        rehypeComponents,
+        {
+          components: {
+            github: GithubCardComponent,
+            note: (x, y) => AdmonitionComponent(x, y, "note"),
+            tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+            important: (x, y) => AdmonitionComponent(x, y, "important"),
+            caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+            warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+          },
         },
       ],
     ],
